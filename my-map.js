a502984ba1054d5aadccf803a7ab8cbc783237f4
vagrant <lauren.dentremont@gmail.com>
2016-10-06 19:49:13 +0000

    updated map function

--- my-map.js
+++ my-map.js
@@ -1,6 +1,6 @@
 map(["ground", "control", "to", "major", "tom"], (elm) => { return elm.length });
 
-function map (arr, elm) {
+function map (arr, callback) {
   var mapArray = [];
   for(var i = 0; i < arr.length; i++)
     length = arr[i].length;
