df87c0c3e9a3368297b482ac9873eae7ec08606b
vagrant <lauren.dentremont@gmail.com>
2016-10-06 17:34:53 +0000

    sorted some arrays

---
+++ array-sort.js
@@ -0,0 +1,26 @@
+var array = [10, 2, 5, 1, 9];
+var sortedArr = array.sort(function (a,b) {
+  return a - b
+  })
+console.log(sortedArr)
+
+
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+var sortStudents = students.sort(function (a,b) {
+  var nameA = a.name.toUpperCase();
+  var nameB = b.name.toUpperCase();
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+  return 0;
+});
+
+console.log(sortStudents)
